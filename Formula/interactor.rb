15341b57a5c96ce4577896d7b1cb553c23a2715a
Elijah Mooring <vehmloewff@gmail.com>
2026-02-12 17:10:01 -0800

    Interactor [0.1.3]

--- Formula/interactor.rb
+++ Formula/interactor.rb
@@ -1,21 +1,26 @@
 class Interactor < Formula
   desc "Browser interactor CLI powered by Playwright"
   homepage "https://github.com/OWNER/REPO"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/OWNER/REPO/releases/download/0.1.2/interactor-0.1.2-darwin-arm64.tar.gz"
-      sha256 "c6df31f817ff28ee7f71979b37d3062a9e2c27d707bcceba267718de6bd981b9"
+      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-darwin-arm64.tar.gz"
+      sha256 "59515259f6cd4c95a2f5b34ab41354ce19f682f49e32eb181d33976159862951"
     else
-      url "https://github.com/OWNER/REPO/releases/download/0.1.2/interactor-0.1.2-darwin-x64.tar.gz"
-      sha256 "8d67da71583939abb4bdc8d6a302101862df1c8df7462399a11025a40c594d83"
+      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-darwin-x64.tar.gz"
+      sha256 "ddc27d09dd7f064940a3fa5d2c60044a0ebcfb5c8263699c725427fbe3f49d07"
     end
   end
 
   on_linux do
-    url "https://github.com/OWNER/REPO/releases/download/0.1.2/interactor-0.1.2-linux-x64.tar.gz"
-    sha256 "00d0f8f2e26e39d2dbb32878ad65694cd3730375f751b56384e241be55eee38b"
+    if Hardware::CPU.arm?
+      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-linux-arm64.tar.gz"
+      sha256 "6064990dd4668329191a2c5f854cea2447daa841c704bc8289af4dee3cdbb0eb"
+    else
+      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-linux-x64.tar.gz"
+      sha256 "23d3955f6a9a4460e287de9789ef900876f510b298a1d111a24416a10df55fa3"
+    end
   end
 
   depends_on "oven-sh/bun/bun"
