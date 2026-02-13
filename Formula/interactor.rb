05d6f1e5be105cecf0cff90322e59a8c0ab809f7
Elijah Mooring <vehmloewff@gmail.com>
2026-02-12 17:18:39 -0800

    Interactor [0.1.3]

--- Formula/interactor.rb
+++ Formula/interactor.rb
@@ -1,25 +1,25 @@
 class Interactor < Formula
   desc "Browser interactor CLI powered by Playwright"
-  homepage "https://github.com/OWNER/REPO"
+  homepage "https://github.com/vehmloewff/interactor"
   version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-darwin-arm64.tar.gz"
-      sha256 "59515259f6cd4c95a2f5b34ab41354ce19f682f49e32eb181d33976159862951"
+      url "https://github.com/vehmloewff/interactor/releases/download/0.1.3/interactor-0.1.3-darwin-arm64.tar.gz"
+      sha256 "891ef7136e5e763e6a05ac0230d7b9987212346ef6f2685f256dd7f493cdc995"
     else
-      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-darwin-x64.tar.gz"
-      sha256 "ddc27d09dd7f064940a3fa5d2c60044a0ebcfb5c8263699c725427fbe3f49d07"
+      url "https://github.com/vehmloewff/interactor/releases/download/0.1.3/interactor-0.1.3-darwin-x64.tar.gz"
+      sha256 "bdac526724e607020f3c1a4c92dba8ce4c998ae3232b1ee047915cd0ddacc39d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-linux-arm64.tar.gz"
-      sha256 "6064990dd4668329191a2c5f854cea2447daa841c704bc8289af4dee3cdbb0eb"
+      url "https://github.com/vehmloewff/interactor/releases/download/0.1.3/interactor-0.1.3-linux-arm64.tar.gz"
+      sha256 "5bbd4b558f0117f1ae571c48f16bebd69ee40254e343fda8e6a214541c9b4c32"
     else
-      url "https://github.com/OWNER/REPO/releases/download/0.1.3/interactor-0.1.3-linux-x64.tar.gz"
-      sha256 "23d3955f6a9a4460e287de9789ef900876f510b298a1d111a24416a10df55fa3"
+      url "https://github.com/vehmloewff/interactor/releases/download/0.1.3/interactor-0.1.3-linux-x64.tar.gz"
+      sha256 "d8e5de2b962dab804f9cabe37adf6c7bca3d395432460ff9572ce2855ec4d559"
     end
   end
 
